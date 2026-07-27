79b88f987423ab219da1cfd67ddcd881a5731058
NChienK18 <Chiennthe187054@fpt.edu.vn>
2026-07-27 08:25:05 +0700

    remove temporary backend js scripts

--- backend/count_tables.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('schema.json', 'utf8'));
-const tables = new Set();
-data.columns.forEach(c => tables.add(c.TableName));
-console.log('Total tables:', tables.size);
-console.log('Tables:', Array.from(tables).sort().join(', '));
